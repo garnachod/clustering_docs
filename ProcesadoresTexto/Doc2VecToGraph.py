6ed7a716ae158633456152ba72a16c0af2a4496d
Daniel Garnacho <garnachod@gmail.com>
2016-06-07 11:13:20 +0200

    Graph word2vec

--- ProcesadoresTexto/Doc2VecToGraph.py
+++ ProcesadoresTexto/Doc2VecToGraph.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
 	#d2v.train("/home/dani/crawler/limpiado.txt", "/home/dani/crawler/train.d2v", method="DM", ides="String")
 	d2v.loadModel("/home/dani/crawler/train.d2v")
 	DG = nx.DiGraph()
+	"""
 	print d2v.doc2vec.cum_table
 	
 	for word in d2v.doc2vec.index2word[:1000]:
@@ -14,5 +15,11 @@ if __name__ == '__main__':
 		for word_, similarity in similares:
 			DG.add_edge(word,word_, weight=similarity)
 
+	"""
+	for doc in d2v.doc2vec.docvecs.doctags:
+		#print doc
+		similares = d2v.doc2vec.docvecs.most_similar(doc, topn=50)
+		for doc_, similarity in similares:
+			DG.add_edge(doc,doc_, weight=similarity)
 
-	nx.write_gexf(DG, "graph.gexf")
+	nx.write_gexf(DG, "graph_docs.gexf")
